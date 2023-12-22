9d7306becc28343ffd1b3b3bbba31478f6954bfb
XargonWan <xargonwan@gmail.com>
2023-12-22 10:16:54 +0100

    repo init [skip ci]

---
+++ automation_tools/update_mame_manifest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git clone https://github.com/XargonWan/RetroDECK --depth=1 RetroDECK
+
+# Creating MAME manifest
+manifest_header="manifest-header.yml"
+mame_module="mame-module.yml"
+mame_manifest="net.retrodeck.mame"
+
+sed -n '/cleanup/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"
+sed -i '/^[[:space:]]*#/d' "$manifest_header"
+sed -i 's/[[:space:]]*#.*$//' "$manifest_header"
+
+sed -i 's/net.retrodeck.retrodeck/net.retrodeck.mame/' "$manifest_header"
+sed -i 's/retrodeck\.sh/mame/' "$manifest_header"
+
+cat "$manifest_header" >> "$mame_manifest"
+cat "$mame_module" >> "$mame_manifest"
+
+rm -rf RetroDECK
