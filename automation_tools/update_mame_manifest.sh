b2d9692b4513a16760f9dbc4633cde0a3db262b1
XargonWan <xargonwan@gmail.com>
2024-01-06 09:19:22 +0100

    WORKFLOW: improved manifest creation

--- automation_tools/update_mame_manifest.sh
+++ automation_tools/update_mame_manifest.sh
@@ -6,13 +6,15 @@ git clone https://github.com/XargonWan/RetroDECK --depth=1 RetroDECK
 manifest_header="manifest-header.yml"
 mame_module="mame-module.yml"
 mame_manifest="net.retrodeck.mame.yml"
+command="/app/bin/mame" 
 
-sed -n '/cleanup/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"
+sed -n '/command/q;p' RetroDECK/net.retrodeck.retrodeck.yml > "$manifest_header"
+echo -e "command: $command\n" >> "$manifest_header"
 sed -i '/^[[:space:]]*#/d' "$manifest_header"
 sed -i 's/[[:space:]]*#.*$//' "$manifest_header"
+sed -n '/finish-args:/,${/cleanup:/q;p;}' RetroDECK/net.retrodeck.retrodeck.yml >> "$manifest_header"
 
 sed -i 's/net.retrodeck.retrodeck/net.retrodeck.mame/' "$manifest_header"
-sed -i 's/retrodeck\.sh/\/app\/bin\/mame/' "$manifest_header"
 
 cat "$manifest_header" > "$mame_manifest"
 cat "$mame_module" >> "$mame_manifest"
